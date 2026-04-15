a1340a69fbab35bdd07b4d5ac726416b8c0dfc40
Lexar Cabrira <lexarcabrira@gmail.com>
2026-04-15 13:05:04 +0800

    fix

--- include/include.js
+++ include/include.js
@@ -1,5 +1,7 @@
-document.querySelectorAll("[includes]").forEach(element => {
-    fetch(element.getAttribute("includes"))
-    .then(res => res.text())
-    .then(data => element.innerHTML = data)
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("[includes]").forEach(element => {
+        fetch(element.getAttribute("includes"))
+        .then(res => res.text())
+        .then(data => element.innerHTML = data)
+    });
 });
